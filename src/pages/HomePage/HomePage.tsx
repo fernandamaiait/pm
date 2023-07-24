55cb1b0433199f4227cbcf8995cc3e28bd594ab5
fernandamaiabettha <fernanda.maia@bettha.com>
2023-07-24 15:18:37 -0300

    feat: add HomePage

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,12 @@
+import TabButton from '../../design-system/TabButton';
+
+export default function HomePage() {
+  return (
+    <div>
+      {/* <div className="font-archivo font-normal">Teste1</div>
+      <div className="font-archivo font-bold">Teste1</div>
+      <div className="font-archivo-narrow">Teste1</div> */}
+      <TabButton index={1} text="Sales Pipeline Settings" />
+    </div>
+  );
+}
