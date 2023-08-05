9b550af032495927da06d1c59ba90630082652b3
fernandamaiabettha <fernanda.maia@bettha.com>
2023-08-05 18:13:31 -0300

    feat: create DS component Switch

---
+++ src/design-system/Switch/Switch.tsx
@@ -0,0 +1,15 @@
+interface ISwitchProps {
+  color: string;
+  active: boolean;
+}
+export default function Switch({ color, active = false }: ISwitchProps) {
+  console.log(active);
+  return (
+    <div
+      className={`bg-${color}-light h-[22px] w-[114px] rounded-[10.5px] flex justify-${
+        active ? 'end' : 'start'
+      }`}>
+      <div className={`bg-${color} w-[42px] rounded-[10.5px] `} />
+    </div>
+  );
+}
