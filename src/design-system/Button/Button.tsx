7a90ccad2ed1adaf4f544e25dc9297442c66cf86
fernandamaiabettha <fernanda.maia@bettha.com>
2023-08-05 18:11:18 -0300

    feat: create DS component Button

---
+++ src/design-system/Button/Button.tsx
@@ -0,0 +1,16 @@
+import { MouseEventHandler } from 'react';
+
+interface IButtonProps {
+  text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
+}
+
+export default function Button({ text, onClick }: IButtonProps) {
+  return (
+    <button
+      className="bg-blue-highlight text-white font-bold text-sm px-[21px] py-[13px] rounded-[34px]"
+      onClick={onClick}>
+      {text}
+    </button>
+  );
+}
