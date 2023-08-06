25a9d1eb0c1574a125463d1182474ba465ad45cc
fernandamaiabettha <fernanda.maia@bettha.com>
2023-08-06 15:52:33 -0300

    feat: create DS index

---
+++ src/design-system/index.ts
@@ -0,0 +1,7 @@
+import Button from './Button/Button';
+import CheckBox from './CheckBox/CheckBox';
+import Switch from './Switch/Switch';
+import TabBar from './TabBar/TabBar';
+import TabButton from './TabButton/TabButton';
+
+export { Button, CheckBox, Switch, TabBar, TabButton };
